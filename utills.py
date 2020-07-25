f55be93de70383a5ba08222dd758bc68b1b89859
marina-magdy <49943493+marina-magdy@users.noreply.github.com>
2020-07-25 08:56:46 +0200

    Add files via upload

---
+++ utills.py
@@ -0,0 +1,21 @@
+
+import mysql.connector as mysql
+import datetime
+
+conn = mysql.connect(host="localhost", user="root", password="Marina98", db="mcdb")
+
+
+def getUser(user):
+    cur = conn.cursor(dictionary=True)
+    qry = "SELECT * FROM `user` WHERE `username`= '{}'".format(user)
+    cur.execute(qry)
+    user = cur.fetchone()
+    return user
+
+
+def save(user_id):
+    cur = conn.cursor(dictionary=True)
+    qry = "INSERT INTO `user_attendance` (user_id , date) VALUES(%s,%s)"
+    cur.execute(qry, (user_id, datetime.datetime.now()))
+    conn.commit()
+    return True
